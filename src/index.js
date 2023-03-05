188e9633a32e9e391759e8a70107d84d24cebc66
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2023-03-05 13:16:25 +0100

    add github pages, favicon and title

--- src/index.js
+++ src/index.js
@@ -9,11 +9,20 @@ import {InformationPart} from "./parts/InformationPart/InformationPart";
 import {ContactPart} from "./parts/ContactPart/ContactPart";
 import {GaleryPart} from "./parts/GaleryPart/GaleryPart";
 import {Footer} from "./parts/Footer/Footer";
+import {Helmet} from 'react-helmet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const TITLE = 'Naše svatba';
+
+const meta = {
+    title: TITLE
+}
 
 root.render(
     <React.StrictMode>
+        <Helmet>
+            <title>{TITLE}</title>
+        </Helmet>
         <Navigation></Navigation>
         <WelcomePart></WelcomePart>
         <OurStoryPart></OurStoryPart>
